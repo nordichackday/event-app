30005f5b6cf773eb5b920f9b40293274b024f9a0
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2016-04-28 22:12:52 +0200

    Parametrized result language

--- common/util/api.js
+++ common/util/api.js
@@ -6,9 +6,9 @@ const api = axios.create({
 });
 
 export default {
-  getArtistsForCurrentYear() {
+  getArtistsForCurrentYear(language = "en") {
     return api.get(
-      '/sparql?query=SELECT%20%3Fhuman%20%3FhumanLabel%20%3Fpicture%20WHERE%20%7B%20%3Fhuman%20wdt%3AP31%20wd%3AQ5%3B%20wdt%3AP1344%20wd%3AQ6015709%20.%20%3Fhuman%20wdt%3AP18%20%3Fpicture%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22%20%7D%20%7D&format=json'
+        '/sparql?query=SELECT%20%3Fhuman%20%3FhumanLabel%20%3Fpicture%20WHERE%20%7B%20%3Fhuman%20wdt%3AP31%20wd%3AQ5%3B%20wdt%3AP1344%20wd%3AQ6015709%20.%20%3Fhuman%20wdt%3AP18%20%3Fpicture%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22' + language + '%22%20%7D%20%7D&format=json'
     );
   }
 }
