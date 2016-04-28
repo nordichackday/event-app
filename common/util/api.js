02412e3e1ebf12da2d228a972f99002d06722e58
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2016-04-28 21:02:04 +0200

    Get real data from Wikidata

--- common/util/api.js
+++ common/util/api.js
@@ -10,7 +10,7 @@ export default {
     return api.get(
         '/sparql?query=SELECT%20?human%20?humanLabel%20WHERE%20%7B%20?human%20wdt:P31%20wd:Q5%20;%20wdt:P1344%20wd:Q6015709%20SERVICE%20wikibase:label%20%7B%20bd:serviceParam%20wikibase:language%20%22en%22%20%7D%20%7D&format=json'
     );
-  }
+  },
 
   getArtistsForLastYear() {
     return api.get(
